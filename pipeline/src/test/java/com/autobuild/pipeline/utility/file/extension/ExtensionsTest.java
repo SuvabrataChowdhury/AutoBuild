d92ac5db060a64dc4f3797e3b8b0c019d9894809
Suvabrata Chowdhury <114484620+SuvabrataChowdhury@users.noreply.github.com>
2025-11-14 22:57:25 +0530

    Test directory reorginization (#66)
    * Initial Design and overall restructuring
    
    * Block Architecture
    
    * Extracted utility out of definition
    
    * Reorginized test directory
    
    ---------
    
    Co-authored-by: I578071 <suvabrata.chowdhury@sap.com>

--- pipeline/src/test/java/com/autobuild/pipeline/utility/file/extension/ExtensionsTest.java
+++ pipeline/src/test/java/com/autobuild/pipeline/utility/file/extension/ExtensionsTest.java
@@ -1,4 +1,4 @@
-package com.autobuild.pipeline.definition.utility.file.extension;
+package com.autobuild.pipeline.utility.file.extension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
