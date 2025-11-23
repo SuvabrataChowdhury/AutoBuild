72509d70667fd32f93b42dda74eb186ee9ce09db
Baibhab Dey <95491588+Baibhab23@users.noreply.github.com>
2025-11-23 18:39:20 +0530

    Implemented pipeline stage modification functionality and update  (#68)
    * Implemented pipeline stage modification functionality and update related tests
    
    * Refactor pipeline validation and controller tests
    
    - Removed the PipelineValidator class as it was deemed unnecessary.
    - Updated PipelineControllerTest to improve test structure and readability.
    - Added tests for creating pipelines with duplicate stages and handling success scenarios.
    - Enhanced PipelineServiceTest with additional tests for modifying pipelines, including handling duplicate stage names and ensuring proper error handling for invalid requests.
    - Cleaned up unused imports and improved overall test coverage for pipeline operations.
    
    * Refactor PipelineService for improved readability and maintainability
    
    ---------
    
    Co-authored-by: Baibhab Dey <baibhab.dey@sap.com>

--- pipeline/src/main/java/com/autobuild/pipeline/utility/file/PipelineFileService.java
+++ pipeline/src/main/java/com/autobuild/pipeline/utility/file/PipelineFileService.java
@@ -5,7 +5,9 @@ import java.util.Map;
 import java.util.UUID;
 
 import com.autobuild.pipeline.definiton.dto.PipelineDTO;
+import com.autobuild.pipeline.definiton.dto.StageDTO;
 import com.autobuild.pipeline.definiton.entity.Pipeline;
+import com.autobuild.pipeline.definiton.entity.Stage;
 
 /**
  * General Interface for any file related operations for pipeline.
@@ -17,4 +19,9 @@ public interface PipelineFileService {
     public Map<UUID, String> createScriptFiles(final PipelineDTO pipeline) throws IOException;
     public void removeScriptFiles(final PipelineDTO pipeline) throws IOException;
     public void removeScriptFiles(final Pipeline pipeline) throws IOException;
+
+    public String createStageScriptFile(final Pipeline pipeline, final StageDTO stage) throws IOException;
+    public void updateStageScriptFile(final Stage stage, final String command) throws IOException;
+    public void removeStageScriptFile(final Stage stage) throws IOException;
+    String readStageScriptFile(final Stage stage) throws IOException;
 }
