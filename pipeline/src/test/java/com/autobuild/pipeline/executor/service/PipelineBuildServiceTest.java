93e4985f29db9d3b57938ff5cc3ed4b3c25fd5e8
Suvabrata Chowdhury <114484620+SuvabrataChowdhury@users.noreply.github.com>
2025-12-03 00:56:53 +0530

    Added pipeline build delete api (#81)
    * Added pipeline build delete api
    
    * ut fixes
    
    ---------
    
    Co-authored-by: I578071 <suvabrata.chowdhury@sap.com>

--- pipeline/src/test/java/com/autobuild/pipeline/executor/service/PipelineBuildServiceTest.java
+++ pipeline/src/test/java/com/autobuild/pipeline/executor/service/PipelineBuildServiceTest.java
@@ -2,6 +2,7 @@ package com.autobuild.pipeline.executor.service;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -26,6 +27,7 @@ import com.autobuild.pipeline.executor.execution.observer.PipelineExecutionObser
 import com.autobuild.pipeline.executor.execution.state.PipelineExecutionState;
 import com.autobuild.pipeline.executor.repository.PipelineBuildRepository;
 import com.autobuild.pipeline.testutility.DummyData;
+import com.autobuild.pipeline.utility.file.PipelineFileService;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -38,6 +40,9 @@ public class PipelineBuildServiceTest {
 
     @Mock
     private PipelineBuildMapper mapper;
+
+    @Mock
+    private PipelineFileService pipelineFileService;
 
     @InjectMocks
     private PipelineBuildService pipelineBuildService;
@@ -129,5 +134,34 @@ public class PipelineBuildServiceTest {
         pipelineBuildService.sentHeartBeat();
 
         verify(newSubscriber, times(1)).send(any(SseEventBuilder.class));
+    }
+
+    @Test
+    public void deletePipelineBuildTest() throws IOException {
+        UUID buildId = UUID.randomUUID();
+
+        PipelineBuild pipelineBuild = DummyData.getPipelineBuild(buildId);
+        pipelineBuild.setCurrentState(PipelineExecutionState.SUCCESS);
+        doReturn(Optional.of(pipelineBuild)).when(repository).findById(any(UUID.class));
+
+        pipelineBuildService.deletePipelineBuild(buildId);
+        
+        verify(pipelineFileService,times(1)).removeLogFiles(eq(pipelineBuild));
+        verify(repository,times(1)).deleteById(eq(buildId));
+
+    }
+
+    @Test
+    public void deletePipelineBuildIllegalStateTest() throws IOException {
+        UUID buildId = UUID.randomUUID();
+
+        PipelineBuild pipelineBuild = DummyData.getPipelineBuild(buildId);
+        doReturn(Optional.of(pipelineBuild)).when(repository).findById(any(UUID.class));
+
+        assertThrows(IllegalStateException.class, () -> pipelineBuildService.deletePipelineBuild(buildId));
+
+        pipelineBuild.setCurrentState(PipelineExecutionState.WAITING);
+
+        assertThrows(IllegalStateException.class, () -> pipelineBuildService.deletePipelineBuild(buildId));
     }
 }
